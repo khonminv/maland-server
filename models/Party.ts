7b0981cdb6da0740db559e3a4fe18fa90a79edaa
권호민 <homin@gwonhomin-ui-MacBookPro.local>
2025-08-01 09:13:18 +0900

    2025-08-01-5

--- models/Party.ts
+++ models/Party.ts
@@ -6,7 +6,7 @@ const partySchema = new mongoose.Schema(
     mapName: { type: String, required: true }, // 예: "리프레"
     title: { type: String, required: true },
     description: String,
-    userId: { type: String, required: true },
+    // userId: { type: String, required: true },
     type: { type: String, enum: ["사냥", "보스", "기타"], default: "기타" }, // 선택지 분류
     maxMember: { type: Number, default: 6 },
     currentMember: { type: Number, default: 1 },
