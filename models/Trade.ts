3b11019459d0da742c52cdb3a1b93b0f5716e6b6
권호민 <homin@gwonhomin-ui-MacBookPro.local>
2025-08-01 09:41:08 +0900

    15

--- models/Trade.ts
+++ models/Trade.ts
@@ -9,7 +9,7 @@ const TradeSchema = new mongoose.Schema({
   isCompleted: { type: Boolean, default: false },
   status: {
     type: String,
-    enum: ["거래가능", "예약중", "거래완료"],
+    enum: ["거래가능", "예약중", "거래완료","대기중"],
     default: "거래가능",
   },
   userId: { type: String, required: true }, // 디스코드 사용자 ID
