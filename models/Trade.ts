78eabdc2aa4433e51597a380f57fdb07cd85e6ba
권호민 <homin@gwonhomin-ui-MacBookPro.local>
2025-08-03 21:42:46 +0900

    2

--- models/Trade.ts
+++ models/Trade.ts
@@ -1,10 +1,9 @@
-// models/Trade.ts
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 const TradeSchema = new mongoose.Schema({
   type: { type: String, enum: ["삽니다", "팝니다"], required: true },
   mapName: { type: String, required: true },
-  subMap: { type: String, required: true }, // 수정됨
+  subMap: { type: String, required: true },
   price: { type: Number, required: true },
   description: { type: String },
   isCompleted: { type: Boolean, default: false },
@@ -13,11 +12,10 @@ const TradeSchema = new mongoose.Schema({
     enum: ["거래가능", "예약중", "거래완료", "대기중"],
     default: "거래가능",
   },
-  userId: { type: String, required: true }, 
-  username: { type: String }, 
-  avatar: { type: String },  
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  username: { type: String },
+  avatar: { type: String },
   createdAt: { type: Date, default: Date.now },
 });
-
 
 export default mongoose.model("Trade", TradeSchema);
